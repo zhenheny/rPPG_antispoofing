056fd6546a9ff36a7e65ab84dc240b1d9db29a2a
zhenheny <zhenheny@12345678.local>
2017-09-26 17:39:57 -0700

    update face detection module

--- run_rppg.py
+++ run_rppg.py
@@ -3,10 +3,16 @@ import numpy as np
 import scipy.signal as signal
 from S2R import *
 
-folder_dir = "/home/zhenheng/datasets/3dmad/frames/Data_01/"
+folder_dir = "/Users/sherry/data_temp/"
 folders = os.listdir(folder_dir)
 for folder in folders:
+	if folder[0] == ".": continue
 	p_blocked = S2R(folder_dir+folder)
-	peakind = signal.find_peaks_cwt(p_blocked, np.arange(1,10))
-	peaks = sorted(p_blocked[peakind])[::-1]
-	print(folder+": "+str(peaks[0]+" "+str(peaks[0]/peaks[1])))
+	f, pxx = signal.periodogram(p_blocked, 50)
+	pxx = np.array([10*math.log10(ele) for ele in pxx])
+	peaks = peakdet(pxx, 0.5, x = None)[0]
+	sort_ind = np.argsort(peaks[:,1])[::-1]
+	peaks = peaks[sort_ind,:]
+	
+	peak_ratio = abs(np.prod(peaks[1:5,1])/pow(peaks[0,1],4))	
+	print(folder+": "+str(peaks[0,1])+" "+str(peak_ratio))
